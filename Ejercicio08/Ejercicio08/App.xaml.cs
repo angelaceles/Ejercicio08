ca3eb82cd7d8c46f02f54e4085486dc342224695
Angela <angela.chancasanampa@tecsup.edu.pe>
2023-05-29 19:35:16 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio08/Ejercicio08/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio08
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            //MainPage = new ExplicitDemo();
+            //MainPage = new ExplicitControlDemo();
+            //MainPage = new ImplicitDemo();
+            //MainPage = new ImplicitControlDemo();
+            //MainPage = new GlobalDemo();
+            //MainPage = new InheritanceDemo();
+            //MainPage = new DynamicDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
